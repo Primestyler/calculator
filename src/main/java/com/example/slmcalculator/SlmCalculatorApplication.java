5c3ca102b130d39a85c4a7e65f0e3c09cb118668
YOUR NAME <YOUR_EMAIL>
2023-05-03 10:29:23 +0200

    initial commit

---
+++ src/main/java/com/example/slmcalculator/SlmCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmCalculatorApplication.class, args);
+    }
+
+}
